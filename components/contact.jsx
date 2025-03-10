987daa1083f904543151860f20b04f36bd6ff44d
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2025-03-10 22:03:16 +0100

    icons style

--- components/contact.jsx
+++ components/contact.jsx
@@ -1,46 +1,26 @@
 import Link from "next/link";
-import { FaInstagramSquare } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
-import { IoIosMail } from "react-icons/io";
-
+import socialLinks from "@/lib/socialLinks";
 export default function Contact() {
   return (
-    <div className="fixed left-3.5 top-[15vh] bottom-[15vh] flex flex-col items-center z-30">
+    <div className="fixed left-3.5 top-[14vh] bottom-[14vh] flex flex-col items-center z-30">
       {/* Barres de décoration */}
       <div className="w-2.5 h-[25vh] bg-amber-500"></div>
-
       {/* Liste des icônes */}
-      <div className="h-[20vh] flex flex-col justify-center space-y-4">
+      <div className="h-[22vh] flex flex-col justify-center space-y-4">
         <ul className="w-full flex flex-col justify-center items-center space-y-4">
-          <li>
-          <Link
-  href={"/"}
-  className="w-8 h-8">
-              <FaInstagramSquare  className="w-6 h-6 text-pink-600 " />
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={"/"}
-              
-               >
-              <FaFacebook className="w-6 h-6 text-blue-600" />
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={"/"}
-              className="w-8 h-8    "
-            >
-              <span className="w-8 h-8 bg-amber-50 flex justify-center items-center rounded-full">
-
-              <IoIosMail className="w-6 h-6 text-black" />
-              </span>
-            </Link>
-          </li>
+          {socialLinks.map(({ href, icon, label }, index) => (
+            <li key={index}>
+              <Link href={href} aria-label={label}>
+                <span
+                  className="w-9 h-9 bg-amber-50 flex justify-center items-center rounded-full shadow-md"
+                                >
+                  {icon}
+                </span>
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
-
       {/* Barres de décoration */}
       <div className="w-2.5 h-[25vh] bg-amber-500"></div>
     </div>
